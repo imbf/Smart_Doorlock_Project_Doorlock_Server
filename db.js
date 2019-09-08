66c5ddb2282aefd4570c3a8bf3334ef80dc373d8
imbf <qowhdwlsdk@gmail.com>
2019-09-09 08:47:14 +0900

    version 6

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 
 module.exports.mobiusdb = mysql.createConnection({
-   host : '168.131.35.103',
+   host : 'localhost',
+   port : 3306,
    user : 'root',
    password: '111111',
    database: 'mobiusdb',
 });
 
 module.exports.doorlockdb = mysql.createConnection({
-   host : '168.131.35.103',
+   host : 'localhost',
    port : 3306,
    user : 'root',
    password: '111111',
