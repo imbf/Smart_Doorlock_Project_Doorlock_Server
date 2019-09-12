868b5f12ad1fee0ea66241aab144821adbcda427
imbf <qowhdwlsdk@gmail.com>
2019-09-12 17:29:21 +0900

    test version4

--- views/hometest.js
+++ views/hometest.js
@@ -1,32 +1,88 @@
 module.exports = {
-   html :function(){
-    return`<!DOCTYPE html>
-<html lang="ko">
-  <head>
-      <style>
-          .container{
-              margin:0;
-              box-sizing:border-box;
-              display:flex;
-              flex-direction:column;
-              width:800px;
-              height:480px;
-              border:1px solid black;
-          }
-          #content-1{
-    
-              text-align:center;
-          }
-      </style>
-  </head>
-  <body class="container">
-      <p id="content-1">
-          ㅎㅇsadfasdnfnsdfmsnadm,vnxcz
-          vnklajdlsfjasdjf
-          asdj
-          vxcnvnzxcvnxcvnzx,.vn
-      </p>
-  </body>
-</html>`;
+   html:function(){
+      return`
+      <!doctype html>
+      <html>
+          <head>
+              <style>
+                  .container{
+                      display:flex;
+                      flex-direction:row;
+                      box-sizing:border-box;  
+                      width:800px;
+                      height:480px;
+                      margin:0px; /*본래는 0px이다.*/
+                  }
+                  #content-1{
+                      display:flex;
+                      flex-direction:column;
+                      width:476px;
+                      box-sizing:border-box;
+                      margin:1px;
+                  }
+                  #content-2{
+                      display:flex;
+                      width:320px;
+                      flex-direction:column;
+                      flex-grow:2;
+                      box-sizing:border-box;
+                      margin:1px;
+                  }
+                  #content-1-1{
+                      height:100px;
+                      box-sizing:border-box;
+                      border:1px solid black;
+                      margin:0px;
+                  }
+                  #content-1-2{
+                      height:188px;
+                      box-sizing:border-box;
+                      border:1px solid black;
+                      margin-top:1px;
+                      margin-bottom:1px;
+                  }
+                  #content-1-3{
+                      height:188px;
+                      box-sizing:border-box;
+                      border:1px solid black;
+                      margin:0px;
+                  }
+                  #content-2-1{
+                      height:269px;
+                      box-sizing:border-box;
+                      border:1px solid black;
+                      margin:0px;
+                  }
+                  #content-2-2{
+                      height:208px;
+                      box-sizing:border-box;
+                      border:1px solid black;
+                      margin-top:1px;
+                  }
+              </style>
+          </head>
+          <body class="container">
+              <div id="content-1">
+                  <div id="content-1-1">
+                      메모
+                  </div >
+                  <div id="content-1-2">
+                      날씨
+                  </div>
+                  <div id="content-1-3">
+                      미세먼지
+                  </div>
+              </div>
+              <div id="content-2">
+                  <div id="content-2-1">
+                      전등제어
+                  </div>
+                  <div id="content-2-2">
+                      문열기
+                  </div>
+              </div>
+          </body>
+      </html>
+      `;
    }
-};
+}
