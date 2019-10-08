efbb0b659f24e4a070a97b7ba7a05e9bc20759ce
imbf <qowhdwlsdk@gmail.com>
2019-10-08 22:14:06 +0900

    Test

--- views/accessList.js
+++ views/accessList.js
@@ -13,26 +13,44 @@ module.exports = {
                      background-size:1600px;
                   }
                   .container{
-                        display:flex;
-                        flex-direction:row;
-                        box-sizing:border-box;  
-                        width:800px;
-                        height:480px;
-                        margin:0px; /*본래는 0px이다.*/
-                  }
-                  #content-1{
-                     display:flex;
-                     margin:20px;
-                     width:760px;
-                     height:440px;
-                     background-color:#fff;
-                     border-radius:3rem;
-                 }
+                     box-sizing:border-box;  
+                     width:800px;
+                     height:480px;
+                     margin:0px; /*본래는 0px이다.*/
+               }
+               #content-1{
+                  display:flex;
+                  flex-direction:column;
+                  margin:20px;
+                  width:760px;
+                  height:440px;
+                  background-color:#fff;
+                  border-radius:3rem;
+               }
+               #content-1-1{
+                 width:760px;
+                 height:60px;
+                 text-align:center;
+                 font-size:2rem;
+                 padding-top:10px;
+                 font-weight:bold;
+                 color:#113366;
+                 box-sizing:border-box;
+                 border-bottom:2px dotted #113366;
+               }
+               #content-1-2{
+                   
+               }
                </style>
             </head>
             <body class="container">
                <div id="content-1">
-
+                  <div id="content-1-1">
+                        출입내역 리스트
+                  </div>
+                  <div id="content-1-2">
+                     ㅎㅇ
+                  </div>
                </div>
             </body>
          </html>
